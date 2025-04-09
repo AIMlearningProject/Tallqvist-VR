fea794fa48a5aded961caf011021a55852943a6f
Ossi Myllymäki <62548204+Ossi-Myllymaki@users.noreply.github.com>
2025-04-09 13:38:17 +0300

    Prefabeja muutettu, jotta ray interactor toimii

--- Assets/New Scripts/LevelData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[System.Serializable]  // Make this class serializable so we can save/deserialize it to/from JSON
-public class LevelData
-{
-    // A list of all objects in the level
-    public List<ObjectData> objects = new List<ObjectData>();
-}
