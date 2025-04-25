e5e382d940df2d83c713b46bc7d057f7ba6e0f37
JamiSoini <jami.2200@gmail.com>
2025-04-25 16:13:36 +0300

    Korjaa RotateCanvas scripti
    Scripti kirjoitettu uusiksi, ja asetettu prefabeihin. Scenessä valmiina olevat prefabit putsattu.

--- Assets/New Scripts/RotateCanvas.cs
+++ Assets/New Scripts/RotateCanvas.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 //K‰‰nt‰‰ prefabeja katsomalla n‰ykvi‰ info tekstej‰ pelaajaa kohti
 
 public class LookAtPlayer : MonoBehaviour
 {
-    public GameObject player;
+    private Transform player;
+
+    void Start()
+    {
+        GameObject cameraObj = GameObject.FindGameObjectWithTag("MainCamera");
+        player = cameraObj.transform;
+    }
 
     void Update()
     {
         transform.LookAt(player.transform);
+
+        Vector3 scale = transform.localScale;
+        scale.x = -Mathf.Abs(scale.x);
+        transform.localScale = scale;
     }
 }
