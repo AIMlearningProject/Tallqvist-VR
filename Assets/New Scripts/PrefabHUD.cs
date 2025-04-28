6aaf0b0193ea997f68d25fdb282f044a617bfc69
JamiSoini <jami.2200@gmail.com>
2025-04-28 12:07:35 +0300

    Korjaa PrefabHUD Scripti
    PrefabHUD Scripti asetettu PrefabSystem GameObjectiin.

--- Assets/New Scripts/PrefabHUD.cs
+++ Assets/New Scripts/PrefabHUD.cs
@@ -1,48 +1,82 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.XR;
 
-//Asettaa käyttäjän valitseman prefabin nimen UI:hin
+//Asettaa käyttäjän valitseman prefabin nimen HUD:iin.
 
 public class PrefabHUD : MonoBehaviour
 {
     public List<GameObject> prefabs;
     private int currentIndex = 0;
 
-    public GameObject GetCurrentPrefab()
+    public GameObject PrefabLabel;
+
+    private InputDevice leftController;
+    private bool stickInUse = false;
+
+    void Start()
     {
-        return prefabs[currentIndex];
+        UpdateLabel();
+        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
     }
-    public void NextPrefab()
+
+    //Ohjaimen lukeminen, tattiohjauksen pitää palautua "0" asentoon ennen seuraavaa tapahtumaa.
+    void Update()
+    {
+        if (!leftController.isValid)
+        {
+            leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+            return;
+        }
+
+        Vector2 primary2DAxis;
+        if (leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out primary2DAxis))
+        {
+            if (!stickInUse)
+            {
+                if (primary2DAxis.x > 0.5f)
+                {
+                    NextPrefab();
+                    stickInUse = true;
+                }
+                else if (primary2DAxis.x < -0.5f)
+                {
+                    PreviousPrefab();
+                    stickInUse = true;
+                }
+            }
+
+            if (stickInUse && Mathf.Abs(primary2DAxis.x) < 0.2f)
+            {
+                stickInUse = false;
+            }
+        }
+    }
+
+        public void NextPrefab()
     {
         currentIndex = (currentIndex + 1) % prefabs.Count;
+        UpdateLabel();
     }
 
     public void PreviousPrefab()
     {
         currentIndex = (currentIndex - 1 + prefabs.Count) % prefabs.Count;
+        UpdateLabel();
     }
 
-    public GameObject PrefabLabel;
-
-    public PrefabManager manager;
-    public void OnEnable()
+    public void UpdateLabel()
     {
-
-        if (currentIndex == 0)
+        var label = PrefabLabel.GetComponent<TMP_Text>();
+        if (label != null && prefabs.Count > 0)
         {
-            PrefabLabel.GetComponent<TMP_Text>().text = "Crate";
-        }
-        else if (currentIndex == 1)
-        {
-            PrefabLabel.GetComponent<TMP_Text>().text = "Shelter";
-        }
-        else
-        {
-            PrefabLabel.GetComponent<TMP_Text>().text = "Toilet";
+            label.text = prefabs[currentIndex].name;
         }
     }
 
-    public int GetCurrentIndex() => currentIndex;
+    public GameObject GetCurrentPrefab()
+    {
+        return prefabs[currentIndex];
+    }
 }
